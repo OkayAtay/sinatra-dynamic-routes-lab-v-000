218f8629cfee367d918e5be3eb5820d5988762b0
Araba Taylor <araba.taylor20@gmail.com>
2018-03-12 02:43:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ class App < Sinatra::Base
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
     if @operation == "add"
-      @operation = +
+      @operation = "+"
     elsif @operation == "subtract"
       @operation = "-"
     elsif @operation == "multiply"
